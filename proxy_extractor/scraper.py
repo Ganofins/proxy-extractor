2cebd691890c037d35a8954110bd9c5b05ae94d5
Ganesh Bagaria <bagariaganesh@gmail.com>
2020-07-01 13:25:33 +0530

    fix UnboundLocalError for proxy dict

--- proxy_extractor/scraper.py
+++ proxy_extractor/scraper.py
@@ -1,15 +1,13 @@
 from bs4 import BeautifulSoup
 
-def _extract_proxies_free_proxy_list_net(html_content, https, proxy_count):
-    """Function to extract proxies from """
+def _extract_proxies_free_proxy_list_net(html_content, https):
+    """Function to extract proxies from https://free-proxy-list.net/"""
     
     proxy_list = []
+    proxy = {}
     soup = BeautifulSoup(html_content, "html.parser")
     table = soup.select("#proxylisttable")[0]
     for each_tr in table.select("tr"):
-        if len(proxy_list) == proxy_count:
-            return proxy_list
-            
         table_td = each_tr.select("td")
         if len(table_td) > 3:
             if https == True:
